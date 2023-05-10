bcf8c803bcf86870e04fb4e4a46b4e9bb08173e1
Pablo <pavel.konstantinov1@mail.ru>
2023-05-11 00:01:41 +0800

    access token

--- src/components/testComp.tsx
+++ src/components/testComp.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import {useState, useEffect} from 'react'
+import {useEffect} from 'react'
 
 export default function TestComponent() {
 
     useEffect(() => {
         axios.get("https://open.faceit.com/data/v4/players/992ec076-71ce-494f-b470-0784c732e205/history?game=csgo&offset=0&limit=20", {
             headers: {
-                Authorization: `Bearer 2b61b257-1978-464b-a367-7eeb52006690`
+                Authorization: `Bearer ${accessToken}`
             }
         })
         .then((result) => {
